53d6657815d25d933f3b06fc0232a2b728a96459
iFooledMe <60869593+iFooledMe@users.noreply.github.com>
2020-05-16 13:08:54 +0200

    Something strange happened in a merge of two branches. This is almost the most recent one so I push it.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-<!-- Custom script to expand and collapse the Top Navbar Submenu  -->
-
-$(document).ready(function () {
-    $(".toggle-button").click(function () {
-        var id = $(this).data("id");
-        $(id).toggle('slow', 'swing');
-    });
-});
-=======
 /* Script to open and close the Main Menu at the Top */
     $(document).ready(function () {
         $(".toggle-button").click(function () {
@@ -15,4 +5,3 @@ $(document).ready(function () {
             $(id).toggle('slow', 'swing');
         });
     });
->>>>>>> 7a68bd2563f76d2d7dc13db0c39a32180a2b394a
